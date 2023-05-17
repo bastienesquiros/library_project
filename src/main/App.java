bf5d2d2f6cb8d22aa7549cb1fb22b75beba2900c
NathLouss <natel@hotmail.com>
2023-05-17 14:11:21 +0200

    app menu subscriber

--- src/main/App.java
+++ src/main/App.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.time.LocalDate;
+import java.time.zone.ZoneRules;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,93 +17,140 @@ import entity.User;
 import entity.Subscriber;
 
 public class App {
+    private static String username;
+
     public static void main(String[] args) {
-        App.run();
+        run();
     }
 
     private static void run() {
-    	signIn();
     	
-    	if(signIn() == true) {
-    		userType();
-    	}
-    }
-    
-    public static boolean signIn() {
-    
-    	Scanner information = new Scanner(System.in);
-    	System.out.println("WELCOME TO THE LIBRARY SYSTEM");
-    	System.out.println("Enter your username: ");
-    	String username = information.nextLine();
-    	System.out.println("Enter your password: ");
-    	String password = information.nextLine();
-    	boolean verification = UserDAO.signIn(username, password);
-    	information.close();
-    	return verification;
+    	if (signIn()) {
+            userType();
+        } else {
+        	System.out.println("Wrong username or password!");
+        }
     	
-    }
-    
-    public static void userType() {
-    	
-    	 System.out.println("Yok");
-//        boolean isLibrarian = false;
-//        boolean isSubscriber = false;
-//     
-//    	Scanner signIn = new Scanner(System.in);
-//
-//
-//        if (verification) {
-//            if (UserDAO.isLibrarian(username)) {
-//                System.out.println("You are logged in as a librarian !");
-//                isLibrarian = true;
-//            } else {
-//                System.out.println("You are logged in as a subscriber !");
-//                isSubscriber = true;
-//            }
-//
-//        } else {
-//            System.out.println("Wrong username or password!");
-//        }
-//  
-//        if (isLibrarian) {
-//            librarianMenu();
-//        } else if (isSubscriber) {
-//            subscriberMenu();
-//        } 
     }
 
-    
+    public static boolean signIn() {
+
+        Scanner information = new Scanner(System.in);
+        System.out.println("WELCOME TO THE LIBRARY SYSTEM");
+        System.out.println("Enter your username: ");
+        username = information.nextLine();
+        System.out.println("Enter your password: ");
+        String password = information.nextLine();
+        
+        boolean verification = UserDAO.signIn(username, password);
+        
+        return verification;
+
+    }
+
+    public static void userType() {
+        if (UserDAO.isLibrarian(username)) {
+            librarianMenu();
+        } else {
+            subscriberMenu();
+        }
+    }
+
     private static void subscriberMenu() {
         System.out.println("A IMPLEMENTER");
     }
 
     private static void librarianMenu() {
-         
-        try (Scanner librarianMenu = new Scanner(System.in)) {
+
+        try (Scanner librarianMenuScan = new Scanner(System.in)) {
             System.out.println("LIBRARIAN MENU");
             System.out.println("1 - Subscriber");
             System.out.println("2 - Document Type");
-            System.out.println("3 - Type");
-            System.out.println("4 - Booking");
-            System.out.println("5 - Rules");
-            
+            System.out.println("3 - Document");
+            // System.out.println("4 - Booking");
+            // System.out.println("5 - Rules");
+
             System.out.println("Enter option number: ");
-            int selectedOption = librarianMenu.nextInt();
-            System.out.println(selectedOption);
-                             
-          
+            int selectedOption = librarianMenuScan.nextInt();
+            
+            List<Integer> optionPossibility = Arrays.asList(1, 2, 3);
+        
+            if (optionPossibility.contains(selectedOption)) {
+                
+                switch (selectedOption) {
+					case 1: crudSubscriberMenu();
+					case 2: crudDocumentTypeMenu();
+					case 3: crudDocumentMenu();
+					// case 4: booking();
+					// case 5: zoneRules();
+                }
+                
+            } else {
+                System.out.println("Option " + selectedOption + " doesn't exist. Choose an option between 1 and 3.");
+            }
+
         } catch (Exception e) {
-            System.out.println("ERROR " + e.getMessage());
-        } finally {
-        	System.out.println("finish");
-        }
+            System.out.println("ERROR :" + e.getMessage());
+        } 
     }
+    
+    public static void crudSubscriberMenu() {
+    	
+    	  try (Scanner subscriberMenuScan = new Scanner(System.in)) {
+              System.out.println("POSSIBLE ACTIONS ON THE SUBSCRIBER");
+              System.out.println("1 - Consult");
+              System.out.println("2 - Add");
+              System.out.println("3 - Modify");
+              System.out.println("4 - Delete");              
+
+              System.out.println("Enter option number: ");
+              int selectedOption = subscriberMenuScan.nextInt();
+              
+              List<Integer> optionPossibility = Arrays.asList(1, 2, 3, 4);
+          
+              if (optionPossibility.contains(selectedOption)) {
+                  
+                  switch (selectedOption) {
+  					case 1: consultSubscriber();
+  					case 2: addSubscriber();
+  					case 3: modifySubscriber();
+  					case 5: deleteSubscriber();
+                  }
+                  
+              } else {
+                  System.out.println("Option " + selectedOption + " doesn't exist. Choose an option between 1 and 4.");
+              }
+
+          } catch (Exception e) {
+              System.out.println("ERROR :" + e.getMessage());
+          } 
+    }
+    
+    public static void consultSubscriber() {
+        System.out.println("ok");
+    }
+    
+    public static void addSubscriber() {
+        
+    }
+    
+    public static void modifySubscriber() {
+        
+    }
+    
+    public static void deleteSubscriber() {
+        
+    }
+    
+    
+    
+    public static void crudDocumentTypeMenu() {
+        
+    }
+    
+    public static void crudDocumentMenu() {
+        
+    }
+    
 }
-//            List<Integer> optionPossibility = Arrays.asList(1, 2, 3, 4, 5);
-//        
-//            if (optionPossibility.contains(selectedOption)) {
-//                System.out.println(selectedOption);
-//            } else {
-//                System.out.println("Option" + selectedOption + "doesn't exist. Choose a option betwen 1 and 5.");
-//            }
 
